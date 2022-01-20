78f0da336fb0a9bbebfbb0707301c8b82faa8b27
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-20 22:06:52 +0300

    Added contact list app

--- ContactListApp/Unittest/contact_test.py
+++ ContactListApp/Unittest/contact_test.py
@@ -10,21 +10,45 @@ class TestContact(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
     def setUp(self):
-            '''
-            Set up method to run before each test cases.
-            '''
+        
+            # Set up method to run before each test cases.
+            
             self.new_contact = Contact("Maxwell","Mishael","0712345678","mishael@ms.com") # create contact object
+            
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Contact.contact_list = []
 
 
     def test_init(self):
-            '''
-            test_init test case to test if the object is initialized properly
-            '''
+            
+           # test_init test case to test if the object is initialized properly
+        
 
             self.assertEqual(self.new_contact.first_name,"Maxwell")
             self.assertEqual(self.new_contact.last_name,"Mishael")
             self.assertEqual(self.new_contact.phone_number,"0712345678")
             self.assertEqual(self.new_contact.email,"mishael@ms.com")
+            
+    def test_save_contact(self):
+            
+            #test_save_contact test case to test if the contact object is saved into
+           # the contact list
+            
+            self.new_contact.save_contact() # saving the new contact
+            self.assertEqual(len(Contact.contact_list),1)
+            
+    def test_save_multiple_contact(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_contact.save_contact()
+            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            test_contact.save_contact()
+            self.assertEqual(len(Contact.contact_list),2)
 
 
 if __name__ == '__main__':
