6c97f2e9506139f71d09edacec44759805cfb324
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-21 08:10:24 +0300

    Updates on contact and contact_test

--- ContactListApp/Unittest/contact.py
+++ ContactListApp/Unittest/contact.py
@@ -12,10 +12,33 @@ class Contact:
         self.phone_number = phone_number
         self.email = email
         
-    def save_contact(self):
-    
+    def save_contact(self):    
         '''
         save_contact method saves contact objects into contact_list
         '''
 
         Contact.contact_list.append(self)
+        
+        
+    def delete_contact(self):    
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
+    
+    
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
